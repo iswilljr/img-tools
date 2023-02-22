ad3c091aef6be1959c1c6292828ff7b47b93d082
iswilljr <iswilljr@gmail.com>
2023-02-21 20:40:59 -0500

    add theme colors and screens to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 const tailwindConfig = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: ['#494F55', '#41474E', '#3A4149', '#333B44', '#2C3540', '#26313C', '#202C39', '#1F2831', '#1E242B'],
+        primary: ['#FEFEFE', '#DCE8F2', '#B8D5EC', '#91C4ED', '#64B5F7', '#55A3E3', '#4A93CF', '#4484B9', '#48769C'],
+        secondary: ['#8495B1', '#6C85AC', '#5376AC', '#4169A6', '#305DA3', '#1F52A1', '#0D47A1', '#194384', '#203E6D'],
+      },
+      screens: {
+        xs: '450px',
+      },
+    },
   },
   plugins: [],
 };
