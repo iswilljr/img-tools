dbc5c40c2590210fdc7edd1c60b906c08fb3a126
iswilljr <iswilljr@gmail.com>
2023-03-02 17:24:01 -0500

    update global styles

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en" className="scroll-smooth">
+    <Html lang="en" className="scroll-smooth [-webkit-tap-highlight-color:transparent]">
       <Head />
       <body className=" overflow-x-hidden bg-dark-11 text-white/90">
         <Main />
