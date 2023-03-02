b7a720f6ac55f65960488fcd4581060b3435b4fe
iswilljr <iswilljr@gmail.com>
2023-03-01 20:31:11 -0500

    move body styles to document file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className="overflow-x-hidden bg-dark-11 text-white/90">
         <Main />
         <NextScript />
       </body>
